1eedb6f232b10eec3d522ea2e3bb6161df983169
Kevin Charles <char0042@umn.edu>
2020-07-20 17:58:51 -0500

    Installed R and test.py w data

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
+# FROM r-base:latest
 FROM rpy2/base-ubuntu:latest
+
+COPY --from=r-base:latest /usr/bin/R /usr/bin/R
 
 WORKDIR /app
 
 COPY requirements.txt /app
 
-RUN sudo ln -s /usr/bin/python3 /usr/bin/python && pip3 install -r /app/requirements.txt
 # Binds command 'python' to 'python3' and install dependencies
+RUN ln -s /usr/bin/python3 /usr/bin/python && pip3 install -r /app/requirements.txt
+
+# RUN apt-get -y update
--- src/test.py
+++ src/test.py
@@ -1,2 +1,24 @@
+import pandas as pd
+import numpy as np
+import rpy2.robjects as ro
+from rpy2.robjects import r, conversion
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 
-print("This is an example")
+
+from rpy2.robjects.packages import importr
+install.packages('devtools')
+# mirt = importr('mirt')
+# stats = importr('stats')
+
+# exam_responses = pd.read_csv('responses.csv')
+# with localconverter(ro.default_converter + pandas2ri.converter):
+#     r_exam_responses = conversion.py2rpy(exam_responses)
+
+# threePL = mirt.mirt(r_exam_responses, 1, itemtype = '3PL', guess = 0.20)
+
+# print((r.coef(threePL, IRTpars=True, simplify=True).rx2('items')))
+
+# params = {'full.scores.SE': True}
+# print(mirt.fscores(threePL, **params))
+
